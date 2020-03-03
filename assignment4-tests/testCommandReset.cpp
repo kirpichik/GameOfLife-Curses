a8f219668ca0b23311b9068bcce20830ff3d6e94
cmhekkala <43660573+cmhekkala@users.noreply.github.com>
2020-03-03 12:24:14 -0800

    Update testCommandReset.cpp

--- assignment4-tests/testCommandReset.cpp
+++ assignment4-tests/testCommandReset.cpp
@@ -1,51 +1,111 @@
-#include <string>
+include <string>
 #include <sstream>
 #include <vector>
 #include "gtest/gtest.h"
 
 #include "game_handler.h"
 
+static void commandReset(const std::vector<std::string>& args,
+                         GameManager& game,
+                         std::ostream& out) {
+  if (args.size() != 2) {
+    if (game.canCreateFieldWithSizes(game.getWidth(), game.getHeight()))
+      game.reset(game.getWidth(), game.getHeight());
+    else {
+      out << "Cannot place game field on this terminal size." << std::endl;
+      return;
+    }
+  } else {
+    int width = stoi(args[0]);
+    int height = stoi(args[1]);
+    if (game.canCreateFieldWithSizes(width, height))
+      game.reset(width, height);
+    else {
+      out << "Cannot place game field on this terminal size." << std::endl;
+      return;
+    }
+  }
+  out << "Field reseted to size " << game.getWidth() << "x" << game.getHeight()
+      << "" << std::endl;
+}
+
+class MockViewHandler : public ViewHandler {
+public:
+    void updateCommandLine(const std::string& commandOutput) override {}
+
+    void updateField(const GameField& field, size_t stepsCount) override {}
+
+    void updateKeyboardCursor(size_t posX, size_t posY) override {}
+
+
+    std::string readCommandInput() override {
+        return "";
+    }
+
+    const InputResult waitForInput(uint8_t timeout) override {
+        return InputResult();
+    }
+
+    bool canCrateFieldWithSizes(size_t width, size_t height) override {
+        return true;
+    }
+
+    void setField(const GameField* field) {
+        ASSERT_TRUE(field != nullptr);
+        if (this->field)
+            delete this->field;
+        this->field = field;
+    }
+
+    void removeField() {
+      field = nullptr;
+    }
+private:
+    const GameField* field = nullptr;
+};
+
 TEST(GameHandler, ValidInput) {
-  TestingListener catcher;
-  GameManager game(10, 10, catcher);
-  std::vector<std::string> args = {3, 3};
+  MockViewHandler viewhandler;
+  GameManager game(10, 10, viewhandler);
+  std::vector<std::string> args = {"3", "3"};
   commandReset(args, game, std::cout);
-  ASSERT_EQ(game.width, 3);
-  ASSERT_EQ(game.height, 3);
+  ASSERT_EQ(game.getWidth(), 3);
+  ASSERT_EQ(game.getHeight(), 3);
 }
 
 TEST(GameHandler, ThreeInputs) {
-  TestingListener catcher;
+  MockViewHandler catcher;
   GameManager game(10, 10, catcher);
-  std::vector<std::string> args = {3, 3, 3};
+  std::vector<std::string> args = {"3", "3", "3"};
   commandReset(args, game, std::cout);
-  ASSERT_EQ(game.width, 10);
-  ASSERT_EQ(game.height, 10);
+  ASSERT_EQ(game.getWidth(), 10);
+  ASSERT_EQ(game.getHeight(), 10);
 }
 
 TEST(GameHandler, OneInput) {
-  TestingListener catcher;
+  MockViewHandler catcher;
   GameManager game(10, 10, catcher);
-  std::vector<std::string> args = {3};
+  std::vector<std::string> args = {"3"};
   commandReset(args, game, std::cout);
-  ASSERT_EQ(game.width, 10);
-  ASSERT_EQ(game.height, 10);
+  ASSERT_EQ(game.getWidth(), 10);
+  ASSERT_EQ(game.getHeight(), 10);
 }
 
 TEST(GameHandler, NoInput) {
-  TestingListener catcher;
+  MockViewHandler catcher;
   GameManager game(10, 10, catcher);
   std::vector<std::string> args = {};
   commandReset(args, game, std::cout);
-  ASSERT_EQ(game.width, 10);
-  ASSERT_EQ(game.height, 10);
+  ASSERT_EQ(game.getWidth(), 10);
+  ASSERT_EQ(game.getHeight(), 10);
 }
 
 TEST(GameHandler, NonsquareInput) {
-  TestingListener catcher;
+  MockViewHandler catcher;
   GameManager game(10, 10, catcher);
-  std::vector<std::string> args = [2, 6];
+  std::vector<std::string> args = {"2", "6"};
   commandReset(args, game, std::cout);
-  ASSERT_EQ(game.width, 2);
-  ASSERT_EQ(game.height, 6);
+  ASSERT_EQ(game.getWidth(), 2);
+  ASSERT_EQ(game.getHeight(), 6);
 }
+                                       
